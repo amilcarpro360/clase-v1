acb9d788e8418bc6865e6361ed09d8d3a0b00488
amilcarpro360 <amilcarvaleromartinez33@gmail.com>
2026-02-11 18:21:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
-self.addEventListener('push', e => {
-    const data = e.data.json();
-    self.registration.showNotification(data.title, {
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
         body: data.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/3449/3449692.png'
-    });
+        icon: 'https://cdn-icons-png.flaticon.com/512/3449/3449692.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/3449/3449692.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
 });
