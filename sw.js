bd2afc95c8ee06982d50e24813934bb4db88690a
amilcarpro360 <amilcarvaleromartinez33@gmail.com>
2026-02-16 19:34:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-// sw.js
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'Actualización', body: 'Nueva actividad en la clase' };
-    const options = {
-        body: data.body,
-        icon: 'https://res.cloudinary.com/dvlbsl16g/image/upload/v1/samples/logo',
-        vibrate: [200, 100, 200],
-        badge: 'https://res.cloudinary.com/dvlbsl16g/image/upload/v1/samples/logo'
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
+    const data = event.data ? event.data.json() : { title: 'Clase', body: 'Nueva actualización' };
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: 'https://res.cloudinary.com/dvlbsl16g/image/upload/v1/logo',
+            vibrate: [200, 100, 200]
+        })
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
